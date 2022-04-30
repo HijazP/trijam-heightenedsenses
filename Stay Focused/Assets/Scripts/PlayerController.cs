650bbc618d5c8fc0b7d3e4a3acfe15cff0dd8db8
HijazP <hanifaliramdani98@gmail.com>
2022-04-30 22:23:09 +0700

    Menambahkan player movement
    Menambahkan aset, script PlayerController.cs, dan chara sudah bisa jalan menggunakan a & d atau panah kiri & panah kanan

---
+++ Stay Focused/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+}
